b4ec230402797cca2704d951da8e1ee8bac79e34
Halls <109175578+HallsRaul@users.noreply.github.com>
2024-10-30 10:16:10 -0300

    MUUUUITA COISA

--- app/src/main/java/com/example/applavitalle/MainActivity.java
+++ app/src/main/java/com/example/applavitalle/MainActivity.java
@@ -10,5 +10,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton card1 = findViewById(R.id.card1);
+        card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.animate()
+                        .scaleX(0.95f)
+                        .scaleY(0.95f)
+                        .setDuration(100)
+                        .withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                v.animate()
+                                        .scaleX(1f)
+                                        .scaleY(1f)
+                                        .setDuration(100)
+                                        .start();
+                            }
+                        })
+                        .start();
+            }
+        });
+
     }
 }
